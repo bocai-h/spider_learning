792a26e1d2960f4243b45da3a10165c86057d139
bocai <woaichibocai_hxb@163.com>
2021-09-28 00:52:14 +0800

    update

--- nft_logo_check.py
+++ nft_logo_check.py
@@ -1,6 +1,6 @@
-import urllib.request
+from urllib.request import urlopen
 
 if __name__ == '__main__':
   for i in range(1, 10001):
     print("i=", i)
-    html = urllib.request.urlopen("https://nftlogo.s3.ap-southeast-1.amazonaws.com/{}.png".format(i));
+    html = urlopen("https://nftlogo.s3.ap-southeast-1.amazonaws.com/{}.png".format(i));
